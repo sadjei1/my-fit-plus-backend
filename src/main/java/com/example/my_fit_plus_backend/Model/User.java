b59c7f5f860ee40be72a38488864c0c16cd044fd
sadjei1 <samuelkyeremeh19@yahoo.com>
2025-10-24 00:15:00 -0600

    additional updates

---
+++ src/main/java/com/example/my_fit_plus_backend/Model/User.java
@@ -0,0 +1,21 @@
+package com.example.my_fit_plus_backend.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
--- src/main/java/com/example/my_fit_plus_backend/MyFitPlusBackendApplication.java
+++ src/main/java/com/example/my_fit_plus_backend/MyFitPlusBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyFitPlusBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyFitPlusBackendApplication.class, args);
+        SpringApplication.run(MyFitPlusBackendApplication.class, args);
 	}
-
 }
