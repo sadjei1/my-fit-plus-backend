e0957a21c89515390ed7d9d61513f41d9d0db5d2
sadjei1 <samuelkyeremeh19@yahoo.com>
2025-10-23 22:42:24 -0600

    first commit

---
+++ src/main/java/com/example/my_fit_plus_backend/MyFitPlusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_fit_plus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitPlusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitPlusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_fit_plus_backend/MyFitPlusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_fit_plus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitPlusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
