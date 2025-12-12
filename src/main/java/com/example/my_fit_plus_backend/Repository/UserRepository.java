3205a29250d0875edc4a3055a021c17485ec21ed
sadjei1 <samuelkyeremeh19@yahoo.com>
2025-12-11 17:36:18 -0700

    update: added finding user by email

--- src/main/java/com/example/my_fit_plus_backend/Repository/UserRepository.java
+++ src/main/java/com/example/my_fit_plus_backend/Repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.my_fit_plus_backend.Repository;
 import com.example.my_fit_plus_backend.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 
 }
